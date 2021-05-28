a6e2be3e7d9bb1212eade220e4f423ca3c0bfe93
David Blue <davidblue@extratone.com>
2021-05-28 16:21:57 -0500

    #212

---
+++ configuration/markdownload/3.0.0_0/PRIVACY.md
@@ -0,0 +1,23 @@
+# Privacy Policy
+
+This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone
+using the MarkDownload browser extension (the Service).
+
+tl;dr: We don't collect anything. Any information we clip from websites remains on your machine.
+
+## Information Collection and Use
+This extension reads data on a website you are visiting when you click the button. The URL, metadata and content are clipped and fed
+through third-party javascript libraries to transofrm it into the Markdown you see in the preview.
+
+None of this data is collected or sent back to any server. Any files you download or text you copy from the extension remains on your
+machine and none of it is collected by me.
+
+The settings of the extension get stored in the browser's local storage and likewise are never collected or transmitted by me.
+
+## Changes to This Privacy Policy
+I may update this Privacy Policy from time to time, for example if new features are developed which require data collection.
+Thus, it might be a good idea to review this page periodically for any changes. Any changes are effective immediately, as they are
+posted on this page. Editing this page constitutes a notification that the Privacy Policy has changed
+
+## Contact Me
+If you have any questions or suggestions about this Privacy Policy, do not hesitate to contact me.
---
+++ configuration/markdownload/3.0.0_0/contentScript/contentScript.js
@@ -0,0 +1,121 @@
+function notifyExtension() {
+    // send a message that the content should be clipped
+    browser.runtime.sendMessage({ type: "clip", dom: content});
+}
+
+function getHTMLOfDocument() {
+    // if the document doesn't have a "base" element make one
+    // this allows the DOM parser in future steps to fix relative uris
+    let baseEl = document.createElement('base');
+
+    // check for a existing base elements
+    let baseEls = document.head.getElementsByTagName('base');
+    if (baseEls.length > 0) {
+        baseEl = baseEls[0];
+    }
+    // if we don't find one, append this new one.
+    else {
+        document.head.append(baseEl);
+    }
+
+    // if the base element doesn't have a href, use the current location
+    if (!baseEl.getAttribute('href')) {
+        baseEl.setAttribute('href', window.location.href);
+    }
+
+    // get the content of the page as a string
+    return document.documentElement.outerHTML;
+}
+
+// code taken from here: https://stackoverflow.com/a/5084044/304786
+function getHTMLOfSelection() {
+    var range;
+    if (document.selection && document.selection.createRange) {
+        range = document.selection.createRange();
+        return range.htmlText;
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        if (selection.rangeCount > 0) {
+            let content = '';
+            for (let i = 0; i < selection.rangeCount; i++) {
+                range = selection.getRangeAt(0);
+                var clonedSelection = range.cloneContents();
+                var div = document.createElement('div');
+                div.appendChild(clonedSelection);
+                content += div.innerHTML;
+            }
+            return content;
+        } else {
+            return '';
+        }
+    } else {
+        return '';
+    }
+}
+
+function getSelectionAndDom() {
+    return {
+        selection: getHTMLOfSelection(),
+        dom: getHTMLOfDocument()
+    }
+}
+
+// This function must be called in a visible page, such as a browserAction popup
+// or a content script. Calling it in a background page has no effect!
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text);
+}
+
+function downloadMarkdown(filename, text) {
+    let datauri = `data:text/markdown;base64,${text}`;
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = datauri;
+    link.click();
+}
+
+function downloadImage(filename, url) {
+
+    /* Link with a download attribute? CORS says no.
+    var link = document.createElement('a');
+    link.download = filename.substring(0, filename.lastIndexOf('.'));
+    link.href = url;
+    console.log(link);
+    link.click();
+    */
+
+    /* Try via xhr? Blocked by CORS.
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'blob';
+    xhr.onload = () => {
+        console.log('onload!')
+        var file = new Blob([xhr.response], {type: 'application/octet-stream'});
+        var link = document.createElement('a');
+        link.download = filename;//.substring(0, filename.lastIndexOf('.'));
+        link.href = window.URL.createObjectURL(file);
+        console.log(link);
+        link.click();
+    }
+    xhr.send();
+    */
+
+    /* draw on canvas? Inscure operation
+    let img = new Image();
+    img.src = url;
+    img.onload = () => {
+        let canvas = document.createElement("canvas");
+        let ctx = canvas.getContext("2d");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+
+        var link = document.createElement('a');
+        const ext = filename.substring(filename.lastIndexOf('.'));
+        link.download = filename;
+        link.href = canvas.toDataURL(`image/png`);
+        console.log(link);
+        link.click();
+    }
+    */
+}
